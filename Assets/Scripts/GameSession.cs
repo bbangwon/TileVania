b4477eea258ad54de1321758314b51e1805da943
youngil <youngil@placeb.com>
2023-10-25 18:31:42 +0900

    씬 유지

--- Assets/Scripts/GameSession.cs
+++ Assets/Scripts/GameSession.cs
@@ -63,6 +63,7 @@ public class GameSession : MonoBehaviour
 
     private void ResetGameSession()
     {
+        FindObjectOfType<ScenePersist>().ResetScenePersist();
         SceneManager.LoadScene(0);
         Destroy(gameObject);
     }
