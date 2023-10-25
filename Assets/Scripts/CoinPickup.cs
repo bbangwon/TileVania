a19c0a360b5ed9edffe716ec7c5900397a602cb6
youngil <youngil@placeb.com>
2023-10-25 15:14:02 +0900

    코인 사운드 이펙트

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
+    [SerializeField] AudioClip coinPickupSFX;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
-        {
+        {            
+            AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position);
             Destroy(gameObject);
         }
     }
